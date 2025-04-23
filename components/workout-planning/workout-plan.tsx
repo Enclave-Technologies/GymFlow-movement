15208729841e3b98d004383c42d805e424935577
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2025-04-23 16:56:42 +0800

    update client-details

--- components/workout-planning/workout-plan.tsx
+++ components/workout-planning/workout-plan.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const WorkoutPlan = ({ client_id }: { client_id: string }) => {
-    console.log("WorkoutPlan", client_id);
+const phases = [{ title: "Session 1" }];
 
-    return (
-        <div className="w-full h-40">
-            {/* TODO: Implement Workout Plan for client {client_id} */}
+const WorkoutPlan = ({ client_id }: { client_id: string }) => {
+  console.log("WorkoutPlan", client_id);
+
+  return (
+    <div className="w-full h-40">
+      {/* TODO: Implement Workout Plan for client {client_id} */}
+      {/* 
+        Phases
+            Sessions
+                Exercises
+      */}
+      {phases.map((phase, index) => (
+        <div key={index}>
+          <p>{phase.title}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default WorkoutPlan;
