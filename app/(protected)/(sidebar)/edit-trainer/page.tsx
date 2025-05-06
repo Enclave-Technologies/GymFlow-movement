db4f9a9853690a6af10f0d066b9b4345ce34ca6f
Avishek Majumder <avishekalex15@gmail.com>
2025-05-06 12:46:53 +0800

    Fix function name for Edit Trainer page

--- app/(protected)/(sidebar)/edit-trainer/page.tsx
+++ app/(protected)/(sidebar)/edit-trainer/page.tsx
@@ -1,21 +1,21 @@
 import EditTrainerForm from "@/components/trainer/edit-trainer-form";
 import { Card, CardContent } from "@/components/ui/card";
 
-export default function AddTrainerPage() {
-  return (
-    <div className="container mx-auto py-6">
-      <div className="mb-8">
-        <h1 className="text-3xl font-bold">Edit Trainer</h1>
-        <p className="text-muted-foreground mt-2">
-          Edit a trainer to update their details.
-        </p>
-      </div>
+export default function EditTrainerPage() {
+    return (
+        <div className="container mx-auto py-6">
+            <div className="mb-8">
+                <h1 className="text-3xl font-bold">Edit Trainer</h1>
+                <p className="text-muted-foreground mt-2">
+                    Edit a trainer to update their details.
+                </p>
+            </div>
 
-      <Card>
-        <CardContent>
-          <EditTrainerForm />
-        </CardContent>
-      </Card>
-    </div>
-  );
+            <Card>
+                <CardContent>
+                    <EditTrainerForm />
+                </CardContent>
+            </Card>
+        </div>
+    );
 }
